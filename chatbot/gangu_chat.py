d504803a0c7cc0c20126d9198861497f39aa5d95
isLinXu <2267379130@qq.com>
2023-05-02 12:50:22 +0800

    [chatbot] update gangu_chat.py

--- chatbot/gangu_chat.py
+++ chatbot/gangu_chat.py
@@ -5,8 +5,19 @@ from pcl_pangu.model import alpha, evolution, mPangu
 
 
 def pangu_inference_chat(message, chat_history,
-                    model='2B6',load='ckpts/onnx_int8_pangu_alpha_2b6/',
-                    backend='onnx-cpu',gmax_tokens=800, output=None):
+                         model='2B6', load='ckpts/onnx_int8_pangu_alpha_2b6/',
+                         backend='onnx-cpu', gmax_tokens=800, output=None):
+    '''
+    pangu_inference_chat
+    :param message:
+    :param chat_history:
+    :param model:
+    :param load:
+    :param backend:
+    :param gmax_tokens:
+    :param output:
+    :return:
+    '''
     set_context(backend=backend)
     bot_message = random.choice(["this is pangu chat test!"])
     chat_history.append((message, bot_message))
@@ -14,4 +25,3 @@ def pangu_inference_chat(message, chat_history,
 
     return alpha.inference(config, input=message,
                            generate_max_tokens=gmax_tokens, output_file=output), chat_history
-
